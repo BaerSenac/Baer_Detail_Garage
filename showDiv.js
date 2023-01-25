6058e47a67748dddaf56cbf886a56d42c953d9a4
Jonathan Baer <jonathanbaeremails@gmail.com>
2023-01-25 00:42:53 -0300

    galery size

---
+++ showDiv.js
@@ -0,0 +1,10 @@
+var buttom = document.getElementById("action-btn");
+
+buttom.addEventListener("click", function() {
+    var container = document.getElementById("container");
+
+    buttom.addEventListener("click", function() {
+        var container = document.getElementById("container")
+        container.classList.toggle("hide");
+    })
+})
